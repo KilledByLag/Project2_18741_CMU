5470d17241b79a5fe6f8d7f990b93fc063169d40
Premsai Peddi <premsaip@rocketmail.com>
2024-12-15 00:26:24 -0500

    Added ONE SIDED neighbor detection, neighbor addition, improved reliablity issues, fixed timing issues

--- content_server.py
+++ content_server.py
@@ -4,6 +4,7 @@ import os
 import sys
 import json
 import time
+import re
 
 
 #Simple Node Structure that stores all details of a particular Node
@@ -13,6 +14,7 @@ class Node:
     def __init__(self, filename):
         # Get details from the conf file
         details = {}
+        
         with open(fr"{filename}") as f:
             for line in f:
                 key, val = line.split("=")
@@ -23,108 +25,173 @@ class Node:
         self.name = details['name']
         self.port = int(details['backend_port'])
         self.host = "localhost"
+
         self.neighbors = {}  
-        self.active_neighbors = []
+        self.active_neighbors = {}
+        self.map = {}
+        self.rank = {}
+
         self.socket = st.socket(st.AF_INET, st.SOCK_DGRAM)
         self.socket.bind((self.host, self.port))
 
-        # Replace set with string or list
+        #Adding neighbor details that the node knows by default
         for i in range(int(details['peer_count'])):
-            self.neighbors[f"neighbor_{i}"] = details[f'peer_{i}']
+            self.neighbors[f"neighbor_{i}"] = {"uuid": details[f'peer_{i}'].split(",")[0].strip(),
+                                               "host": details[f'peer_{i}'].split(",")[1].strip(),
+                                               "backend_port": int(details[f'peer_{i}'].split(",")[2].strip()),
+                                               "metric": int(details[f'peer_{i}'].split(",")[3].strip())}
 
         self.event = thr.Event()
+        self.lock =  thr.Lock()
+        self.NODE_DEATH_TIMEOUT = 5 #Death Declaration threshold
+        self.CLEANUP_PERIODICITY = 0.5 #Cleanup thread check periddicity
+        self.SERVER_WAIT_TIME = 1 #Server Socket Recieve timeout - Raises exception which is ignored, and tries again
+        self.CLIENT_MESSAGE_PERIODICITY = 0.1 #Client message periodicity
             
 
     # Show details of node
     def show_details(self, param):
         print(getattr(self, param))
 
-    def get_message(self):
+    def get_message(self, metric):
         message = {
             "name": self.name,
             "uuid": self.uuid,
-            "port": self.port,
-            "neighbors": list(self.neighbors.values())  # Convert neighbors to list
+            # "port": self.port,
+            "mydistancetoyou": metric
+            # "neighbors": self.neighbors,
         }
         return message
     
+    def update_details(self, message_details):
+
+        def update_active_neighbors(message, addr):
+            with self.lock:
+                neighbor_node_name, neighbor_node_uuid, metric_to_me = message["name"], message["uuid"], message["mydistancetoyou"]
+                # neighbor_node_neighbors = message["neighbors"]
+
+                if neighbor_node_name in self.active_neighbors:
+                    self.active_neighbors[neighbor_node_name]["time"] = time.time()  
+
+                else:
+                    self.active_neighbors[neighbor_node_name] = {"uuid": neighbor_node_uuid,
+                                                "host": addr[0],
+                                                "backend_port": addr[1],
+                                                "metric": metric_to_me,
+                                                "time": time.time()
+                                                }
+                    
+
+        def update_map():
+            pass
+        
+        def update_rank():
+            pass
+
+
+
+        """
+        Input Args : Message, Message "from" address
+        Function : Updates Active Neigbors, Rank, and Map of the node
+
+        """
+        message, addr = message_details
+        message = json.loads(message.decode('utf-8'))
+
+        #Update Active Neighbors
+        update_active_neighbors(message, addr)
+        
+
+
     # Start as a server
     def start_server(self):
-        
         while not self.event.is_set():
             try:
+                self.socket.settimeout(self.SERVER_WAIT_TIME)  # Ensure it doesn't block forever
                 message, addr = self.socket.recvfrom(1024)
-
-                if not message:
-                    break
-                message = message.decode('utf-8')
-                print(message)
-
-                # If new neighbor, add it with a timestamp
-                found = False
-                for i, neighbor in enumerate(self.active_neighbors):
-                    if neighbor[0] == addr[0] and neighbor[1] == addr[1]:
-                        self.active_neighbors[i] = (addr[0], addr[1], time.time())
-                        found = True
-                        break
-                if not found:
-                    self.active_neighbors.append((addr[0], addr[1], time.time()))
-
-                # print(self.active_neighbors)
-                # print(message)
-            except ConnectionResetError as e:
+                if message:
+                    self.update_details((message, addr))
+            except st.timeout:
+                continue  # Timeout ensures the loop remains responsive
+            except ConnectionResetError:
                 pass
-            self.event.wait(1)
-
-
-    def cleanup_neighbors(self, timeout=10):
-        while not self.event.is_set():
-            current_time = time.time()
-            self.active_neighbors = [
-                (ip, port, timestamp) for (ip, port, timestamp) in self.active_neighbors 
-                if current_time - timestamp < timeout
-            ]
-            time.sleep(1)  # Run cleanup every second
-            self.event.wait(1)
 
     
-    def add_neighbors(self):
-        pass
+    def add_neighbors(self, user_input):
+        # Update the regex pattern to account for quotes around the host
+        match_pattern = r'uuid=([\w-]+)\s+host=([\w.-]+)\s+backend_port=(\d+)\s+metric=(\d+)'
+
+        match = re.search(match_pattern, user_input)
+
+        if match:
+            new_node = {
+                'uuid': match.group(1),
+                'host': match.group(2),
+                'backend_port': int(match.group(3)),
+                'metric': int(match.group(4))
+            }
+        else:
+            print("Addneighbor command format wrong!")
+
+            with self.lock:
+                self.neighbors[f"neighbor_{len(self.neighbors)}"] = new_node
 
     # Start as client
     def start_client(self):
         while not self.event.is_set():
             
             try:
-                message = json.dumps(self.get_message()).encode('utf-8')
-                
-                for neighbor in self.neighbors.values():
-                    peer_uuid, host, port, _ = neighbor.split(", ")
+                # message = json.dumps(self.get_message()).encode('utf-8')
+                for neighbor in self.neighbors:
                     
+                    host, port, metric_to_neighbor = self.neighbors[neighbor]["host"], self.neighbors[neighbor]["backend_port"], self.neighbors[neighbor]["metric"]
+                    
+                    message = json.dumps(self.get_message(metric_to_neighbor)).encode('utf-8')
+
                     self.socket.sendto(message, (host, int(port)))
                 
-                time.sleep(2)  # Send message every 5 seconds
+                self.event.wait(0.1)
             except Exception as e:
                 pass
-            self.event.wait(1)
-    
+            
+    def extract_details(self, command):
+        if command == "neighbors":
+            with self.lock:
+                neighbors = {neighbor: {key: value for key, value in details.items() if key != "time"} for neighbor, details in self.active_neighbors.items()}
+                return {"neighbors": neighbors}
+        
+        if command == "uuid":
+            return {"uuid":self.uuid}
+            
+
     def commands(self):
         while not self.event.is_set():
             try:
-                user_input = input("Enter command (type 'exit' to quit): ")
-                if user_input == "exit":
+                user_input = input("Enter command (type 'kill' to quit): ")
+                if user_input == "kill":
                     self.event.set()
                     break
-                elif user_input == "uuid":
-                    print(self.uuid)
-                elif user_input == "active_neighbors":
-                    print(self.active_neighbors)
+                elif user_input.startswith("addneighbor"):
+                    self.add_neighbors(user_input)
                 else:
-                    print("Unknown command. Please try again.")
+                    (print(self.extract_details(user_input)))
+
             except EOFError:
                 self.event.set()
                 break
             self.event.wait(1)
+
+    def cleanup_details(self):
+        while not self.event.wait(timeout=self.CLEANUP_PERIODICITY):
+            removal_list = []
+            with self.lock:
+                for neighbor in self.active_neighbors:
+                    neighbor_last_active = self.active_neighbors[neighbor]['time']
+                    if time.time() - neighbor_last_active > self.NODE_DEATH_TIMEOUT:
+                        removal_list.append(neighbor)
+                for neighbor in removal_list:
+                    del self.active_neighbors[neighbor]
+
 
 
 def main():
@@ -133,7 +200,7 @@ def main():
 
         server_thread = thr.Thread(target=node.start_server)
         client_thread = thr.Thread(target=node.start_client)
-        cleanup_thread = thr.Thread(target=node.cleanup_neighbors)
+        cleanup_thread = thr.Thread(target=node.cleanup_details)
         command_thread = thr.Thread(target=node.commands)
 
         server_thread.start()
@@ -141,12 +208,10 @@ def main():
         cleanup_thread.start()
         command_thread.start()
 
-        # Wait for the command thread to finish
         command_thread.join()
-        print("Exiting program.")
         server_thread.join()
         client_thread.join()
-        cleanup_thread.join()
+        # cleanup_thread.join()
         
 
 if __name__ == "__main__":
